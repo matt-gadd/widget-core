33d1b744486f76d48cd221b0f99d5974e8001df0
Matt Gadd <mgadd@sitepen.com>
2017-04-10 17:06:07 +0100

    move RegistryHandler to separate module

--- src/WidgetBase.ts
+++ src/WidgetBase.ts
@@ -19,7 +19,8 @@ import {
 	PropertiesChangeEvent,
 	HNode
 } from './interfaces';
-import WidgetRegistry, { WIDGET_BASE_TYPE } from './WidgetRegistry';
+import { WIDGET_BASE_TYPE } from './WidgetRegistry';
+import RegistryHandler from './RegistryHandler';
 
 export { DiffType };
 
@@ -119,65 +120,6 @@ export function handleDecorator(handler: (target: any, propertyKey?: string) => 
  */
 function isHNodeWithKey(node: DNode): node is HNode {
 	return isHNode(node) && (node.properties != null) && (node.properties.key != null);
-}
-
-export class RegistryHandler extends Evented {
-	private _registries: { handle?: any, registry: WidgetRegistry }[];
-
-	constructor() {
-		super();
-		this._registries = [];
-	}
-
-	add(registry: WidgetRegistry) {
-		this._registries.unshift({ registry });
-	}
-
-	remove(registry: WidgetRegistry): boolean {
-		return this._registries.some((registryWrapper, i) => {
-			if (registryWrapper.registry === registry) {
-				registry.destroy();
-				this._registries.splice(i, 1);
-				return true;
-			}
-			return false;
-		});
-	}
-
-	replace(original: WidgetRegistry, replacement: WidgetRegistry): boolean {
-		return this._registries.some((registryWrapper, i) => {
-			if (registryWrapper.registry === original) {
-				original.destroy();
-				registryWrapper.registry = replacement;
-				return true;
-			}
-			return false;
-		});
-	}
-
-	has(widgetLabel: string): boolean {
-		return this._registries.some((registryWrapper) => {
-			return registryWrapper.registry.has(widgetLabel);
-		});
-	}
-
-	get(widgetLabel: string): WidgetConstructor | null {
-		for (let i = 0; i < this._registries.length; i++) {
-			const registryWrapper = this._registries[i];
-			const item = registryWrapper.registry.get(widgetLabel);
-			if (item) {
-				return item;
-			}
-			else if (!registryWrapper.handle) {
-				registryWrapper.handle = registryWrapper.registry.on(`loaded:${widgetLabel}`, () => {
-					this.emit({ type: 'invalidate' });
-					registryWrapper.handle.destroy();
-					registryWrapper.handle = undefined;
-				});
-			}
-		}
-		return null;
-	}
 }
 
 /**
