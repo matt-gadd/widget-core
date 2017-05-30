461c5784e697987ddd7964b86eea07fe05ffc669
Matt Gadd <mgadd@sitepen.com>
2017-05-30 13:08:31 +0100

    use latest maquette

--- src/util/DomWrapper.ts
+++ src/util/DomWrapper.ts
@@ -13,42 +13,20 @@ export type DomWrapper = Constructor<WidgetBase<DomWrapperProperties>>;
 
 export function DomWrapper(domNode: Element, options: DomWrapperOptions = {}): DomWrapper {
 	return class extends WidgetBase<DomWrapperProperties> {
-		private _vNode: VNode;
-		private _firstRender = true;
 
 		protected onElementCreated(element: Element, key: string) {
-			element.parentNode && element.parentNode.replaceChild(domNode, element);
-			this._vNode.domNode = domNode;
-			this._firstRender = false;
 			options.onAttached && options.onAttached();
-			this.invalidate();
 		}
 
 		public __render__() {
-			this._vNode = super.__render__() as VNode;
-			(this._vNode.properties as VirtualDomProperties).domNode = domNode;
-			return this._vNode;
-		}
-
-		private _getInitialProperties(properties: DomWrapperProperties): any {
-			return Object.keys(properties).reduce((obj: any, key: string) => {
-				const prop = properties[key];
-				if (key === 'classes' || key === 'styles') {
-					obj[key] = {};
-				}
-				else if (typeof prop === 'function') {
-					obj[key] = prop;
-				}
-				else {
-					obj[key] = undefined;
-				}
-				return obj;
-			}, {});
+			const vNode = super.__render__() as VNode;
+			vNode.domNode = domNode;
+			return vNode;
 		}
 
 		protected render() {
-			const properties = this._firstRender ? this._getInitialProperties(this.properties) : this.properties;
-			properties.bind && delete properties.bind;
+			const properties: any = this.properties;
+			delete properties['bind'];
 			return v(domNode.tagName, { ...properties, key: 'root' });
 		}
 	};
